cf439a4cdcd68dc9cb76352d68b66b97e94b4dba
Jeremy <jmcphers0207@gmail.com>
2026-07-10 18:12:51 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynamic Tintz Business Hub V1
+
+Upload the complete contents of this folder to a GitHub repository. In GitHub, open Settings > Pages, select Deploy from a branch, choose main and /(root), then save. Open the published URL in Safari on iPhone and use Share > Add to Home Screen.
---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='dt-v1';const A=['./','./index.html','./logo.png','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
