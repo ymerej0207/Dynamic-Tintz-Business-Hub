fb810de007a8e41bc5e68e4e7337c29c1e7e842c
Jeremy <jmcphers0207@gmail.com>
2026-08-19 08:56:22 -0500

    ability to share dimensions with customers

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 /*
  * Dynamic Tintz OS service worker
- * Release: 7.8.5-inventory-color-thresholds
+ * Release: 7.8.6-window-measurements
  */
 
-const CACHE_PREFIX = "dynamic-tintz-v7.8.5-";
-const CACHE_NAME = `${CACHE_PREFIX}inventory-color-thresholds`; 
+const CACHE_PREFIX = "dynamic-tintz-v7.8.6-";
+const CACHE_NAME = `${CACHE_PREFIX}window-measurements`; 
 
 const APP_SHELL = [
   "./",
