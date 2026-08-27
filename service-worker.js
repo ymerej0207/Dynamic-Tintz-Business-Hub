210a8354044331bed55eb07d44ccd40f59c77fbf
Jeremy <jmcphers0207@gmail.com>
2026-08-27 13:18:52 -0500

    bug fix with quoting system

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 /*
  * Dynamic Tintz OS service worker
- * Release: 7.10.4-full-film-library
+ * Release: 7.10.7-complete-safe
  */
 
-const CACHE_PREFIX = "dynamic-tintz-v7.10.4-";
-const CACHE_NAME = `${CACHE_PREFIX}full-film-library`; 
+const CACHE_PREFIX = "dynamic-tintz-v7.10.7-";
+const CACHE_NAME = `${CACHE_PREFIX}complete-safe`; 
 
 const APP_SHELL = [
   "./",
